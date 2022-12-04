dc4a4a279a9266b7f408ff5c85fa64cdaa266235
Mohit <mohit.paudyal@gmail.com>
2022-12-04 13:33:45 -0600

    adds sb for request-features form

--- components/Forms/AddFeatureForm.stories.tsx
+++ components/Forms/AddFeatureForm.stories.tsx
@@ -11,7 +11,20 @@ export default {
 
 const Template: ComponentStory<typeof AddFeatureForm> = (args) => <AddFeatureForm {...args} />
 
+const defaultInputRowsAllowed = 4;
+const requestFeatureEmailInputLabel = 'Email Address';
+const requestFeatureInputNoErrMsg = 'Please add more details';
+const requestFeatureInputLabel = 'How can we make the application better?';
+const requestFeatureInputErrMsg = 'All submitted comments will remain anonymous.';
+const requestFeatureEmailInputHelper = 'Participation is 100 % free and voluntary.';
+
 export const AddFeatureFormDefault = Template.bind({});
+
 AddFeatureFormDefault.args = {
-    
+    requestFeatureInputLabel: requestFeatureInputLabel,
+    defaultInputRowsAllowed: defaultInputRowsAllowed,
+    requestFeatureInputNoErrMsg: requestFeatureInputNoErrMsg,
+    requestFeatureInputErrMsg: requestFeatureInputErrMsg,
+    requestFeatureEmailInputLabel: requestFeatureEmailInputLabel,
+    requestFeatureEmailInputHelper: requestFeatureEmailInputHelper,
 }
