ae81a5e6044c281a3593f74a219996c19188d808
Mohit <mohit.paudyal@gmail.com>
2022-12-08 08:28:19 -0600

    final touches for feedback form

--- components/Home/BodyHeaderContent.tsx
+++ components/Home/BodyHeaderContent.tsx
@@ -1,38 +1,30 @@
-import Text from "../Typography/Text";
+import { Typography } from "@mui/material";
 
 const BodyHeaderContent: React.FC = () => {
   return (
     <>
       <main>
-        <Text variant="h4" gutterBottom={true}>
+        <Typography variant="h4" gutterBottom={true}>
           Product Feedback
-        </Text>
-        <Text variant="body2" gutterBottom={true}>
+        </Typography>
+        <Typography variant="body1" gutterBottom={true}>
           Climate strives to bring the best experience during your item
           management.
-        </Text>
-        <Text variant="body2" gutterBottom={false}>
+        </Typography>
+        <Typography variant="subtitle2" color="textSecondary" gutterBottom={false}>
           With a potential to impact billions, Climate welcomes your feedback on
           its products.
-        </Text>
+        </Typography>
         <br />
-        <Text variant="body2" gutterBottom={true}>
-          Begin by using the form below. Submitting this form does not guarentee
-          a response from us. However, we do read each feedback carefully. To
-          keep yourself updated with the changes in your feeback and our
-          response, please be sure to add your email address during your form
-          submission process.
-        </Text>
-        <br />
-        <Text variant="body2" gutterBottom={true}>
-          Please use the form below for your feedback. We read all feedback
-          carefully, however due to the nature of feedbacks, we would be unable
+        <Typography variant="h6" gutterBottom={true}>
+          Please fill the form below.
+        </Typography>
+        <Typography>
+          We read all feedback carefully, however due to the nature of feedbacks, we would be unable
           to respond to each submission individually. If you provide your email
           address, you agree that we may contact you to better understand the
           comments you submitted.
-        </Text>
-        <br />
-        <br />
+        </Typography>
         <br />
       </main>
     </>
