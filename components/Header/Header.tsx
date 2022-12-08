c5b73271759b15d63a890b729949225b6d8f2766
sujan-dahal <dahalsujanusa@gmail.com>
2022-12-07 19:29:33 -0600

    add theme and render hooks

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,15 @@
+import React, { useEffect, useRef } from "react";
+import { debounce } from "@mui/material";
 import Link from "next/link";
 import styles from "../../styles/Header.module.css";
 
-const Header: React.FC = ({
-  toggleDrawer,
-}) => {
+const Header: React.FC = ({ toggleDrawer }) => {
+  const toggle = useRef(null);
+  let wat = useRef(null);
+  useEffect(() => {
+    toggle.current = debounce(wat ? toggleDrawer : () => {}, 1000);
+  });
+
   return (
     <div>
       <Link
@@ -13,6 +19,16 @@ const Header: React.FC = ({
       >
         Skip to Footer
       </Link>
+      <div
+        onMouseEnter={() => {
+          toggle.current?.();
+        }}
+        onMouseOut={() => {
+          toggle.current?.();
+        }}
+      >
+        Hi
+      </div>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import createEmotionCache from "../utility/createEmotionCache";
+import lightTheme from "../styles/theme/lighttheme";
+const clientSideEmotionCache = createEmotionCache();
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CacheProvider value={clientSideEmotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
