7b12deb50ff285ae7a4c935991e515570ef5e8a2
mohit paudyal <mohit.paudyal@gmail.com>
2022-12-10 05:08:05 -0600

    Adding chart for the dashboard components

--- containers/SplashPage/SplashMainPage.tsx
+++ containers/SplashPage/SplashMainPage.tsx
@@ -3,8 +3,12 @@ import React, { useState } from "react";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import FormHelperText from "@mui/material/FormHelperText";
-import { Divider, Select,Typography } from "@mui/material";
+import { Divider, Select, Typography } from "@mui/material";
 import KitchenRoundedIcon from "@mui/icons-material/KitchenRounded";
+import GarageRoundedIcon from "@mui/icons-material/GarageRounded";
+import Inventory2RoundedIcon from "@mui/icons-material/Inventory2Rounded";
+import ShuffleRoundedIcon from "@mui/icons-material/ShuffleRounded";
+import SampleChart from "../../components/Chart/SampleChart";
 
 const options = [
   { value: "24", display: "24 hours", helperText: "24 hours" },
@@ -17,48 +21,151 @@ const storageLocations = [
   {
     id: "1",
     title: "Kitchen",
-    icon: 'KitchenRoundedIcon',
+    icon: "KitchenRoundedIcon",
     iconProps: {
       width: 150,
       height: 150,
     },
     desc: "Expired Items",
     expiredItemsCount: 12,
+    chartInfo: {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      datasets: [
+        {
+          label: "Expiry Items",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 0, 3, 2, 6, 9, 4, 6, 2, 2, 1, 7],
+        },
+      ],
+    },
   },
   {
     id: "2",
     title: "Garage",
-    icon: 'KitchenRoundedIcon',
+    icon: "GarageRoundedIcon",
     iconProps: {
       width: 150,
       height: 150,
     },
     desc: "Perishable Items",
     expiredItemsCount: 2,
+    chartInfo: {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      datasets: [
+        {
+          label: "Perishable Items",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [1, 1, 3, 2, 2, 0, 0, 4, 2, 2, 1, 0],
+        },
+      ],
+    },
   },
   {
     id: "1",
     title: "Storage",
-    icon: 'KitchenRoundedIcon',
+    icon: "Inventory2RoundedIcon",
     iconProps: {
       width: 150,
       height: 150,
     },
     desc: "Unopened Items",
     expiredItemsCount: 22,
+    chartInfo: {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      datasets: [
+        {
+          label: "Unopened Items",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [1, 1, 3, 2, 2, 0, 0, 4, 2, 2, 1, 0],
+        },
+      ],
+    },
   },
   {
     id: "1",
     title: "Mess Drawer",
-    icon: 'KitchenRoundedIcon',
+    icon: "ShuffleRoundedIcon",
     iconProps: {
       width: 150,
       height: 150,
     },
     desc: "Untouched Items",
     expiredItemsCount: 6,
+    chartInfo: {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      datasets: [
+        {
+          label: "Untouched Items",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 0, 1, 1, 1, 0, 4, 3, 1, 0, 0, 2],
+        },
+      ],
+    },
   },
 ];
+
+const ICON_GEN: any = {
+  KitchenRoundedIcon: <KitchenRoundedIcon />,
+  GarageRoundedIcon: <GarageRoundedIcon />,
+  Inventory2RoundedIcon: <Inventory2RoundedIcon />,
+  ShuffleRoundedIcon: <ShuffleRoundedIcon />,
+};
 
 const SplashMainPage = () => {
   const [value, setValue] = useState<any>({
@@ -74,6 +181,10 @@ const SplashMainPage = () => {
       value: newVal?.value,
       helperText: newVal?.helperText,
     });
+  };
+
+  const generateIcon = (title: string) => {
+    return ICON_GEN[title];
   };
 
   return (
@@ -123,7 +234,7 @@ const SplashMainPage = () => {
                   }}
                 >
                   <Typography variant="h5" gutterBottom>
-                    <KitchenRoundedIcon /> {x.title}
+                    {generateIcon(x.icon)} {x.title}
                   </Typography>
                   <Typography variant="body1" gutterBottom lineHeight="1.6">
                     {" "}
@@ -133,6 +244,10 @@ const SplashMainPage = () => {
                   <Typography variant="h5" gutterBottom lineHeight="1.6">
                     {x.expiredItemsCount}
                   </Typography>
+                  <SampleChart
+                    labels={x.chartInfo?.labels}
+                    datasets={x.chartInfo?.datasets}
+                  />
                 </Box>
               </>
             );
