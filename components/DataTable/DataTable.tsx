222ae1e40797ea90ce28dae82a99898264805c7d
sujan-dahal <dahalsujanusa@gmail.com>
2022-12-10 19:40:09 -0600

    add some breadcrumbs

--- components/DataTable/DataTable.tsx
+++ components/DataTable/DataTable.tsx
@@ -1,23 +1,38 @@
-import React, { useState, useMemo, useRef, useCallback } from "react";
-import { AgGridReact } from "ag-grid-react";
+import React, { useState, useMemo, useRef, useCallback, memo } from "react";
+import { AgGridReact, AgGridReactProps } from "ag-grid-react";
 import { Box } from "@mui/material";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-const DataTable = () => {
+interface DataTableProps extends AgGridReactProps {
+  rowData?: any[];
+  columns?: any[];
+}
+
+const DataTable = ({
+  rowData,
+  columns,
+}: DataTableProps) => {
   const gridRef = useRef();
   // some sample rowData
-  const [rowData] = useState([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxster", price: 72000 },
-  ]);
+
+
+
+
 
   // columnNames
-  const [columnDefs] = useState([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
+  const [columnDefs] = useState(columns ?? [
+    { field: "id", headerName: "ID", width: 70, },
+    { field: "name", headerName: "Name", width: 130,  resizable: true, flex: 1 },
+    { field: "description", headerName: "Description", width: 130 },
+    { field: "price", headerName: "Price", width: 130 },
+    { field: "quantity", headerName: "Quantity", width: 130 },
   ]);
+
+  const wat = useMemo(() => [...new Array(10000)].map((_, i) => [...columnDefs.map((c, j) => {
+    return { [c.field]: c.createDefaultValue?.(i, j) };
+  })].reduce((a, b) => ({ ...a, ...b }), {})), [columnDefs]);
+ 
+  const [rowDataState] = useState(rowData ?? wat);
 
   // columnProperties
   const defaultColumnDefs = useMemo(
@@ -33,12 +48,15 @@ const DataTable = () => {
   return (
     <Box
       className="ag-theme-alpine"
-      height="40rem"
+      sx={{
+        minHeight: "20rem",
+        minWidth: "20rem",
+      }}
     >
       <AgGridReact
         className="ag-theme-alpine"
         ref={gridRef}
-        rowData={rowData}
+        rowData={rowDataState}
         columnDefs={columnDefs}
         defaultColDef={defaultColumnDefs}
         animateRows={true}
--- pages/stuff.tsx
+++ pages/stuff.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import BasicBreadcrumbs from "../components/BreadCrumbs/BreadCrumbs";
 import DataTable from "../components/DataTable/DataTable";
 
 const Stuff = () => {
@@ -8,12 +9,60 @@ const Stuff = () => {
         display: "flex",
         flexDirection: "column",
         flexGrow: 1,
-        backgroundColor: "yellow",
-        padding: '1rem'
+        padding: "1rem",
+        height: "100%",
       }}
     >
-      <DataTable 
-      />
+      <Box>
+        <Typography variant="h4">Stuff</Typography>
+        <BasicBreadcrumbs />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          height: "40rem",
+        }}
+      >
+        <DataTable
+          columns={[
+            {
+              field: "id",
+              headerName: "ID",
+              width: 70,
+              createDefaultValue: (i: any, j: any) : string => [i, j].join(),
+            },
+            {
+              field: "name",
+              headerName: "Name",
+              width: 130,
+              resizable: true,
+              flex: 1,
+              createDefaultValue: (i: any, j: any) : string => [i, j].join(),
+            },
+            {
+              field: "description",
+              headerName: "Description",
+              width: 130,
+              suppressSizeToFit: true,
+              createDefaultValue: (i: any, j: any) : string => [i, j].join(),
+            },
+            {
+              field: "price",
+              headerName: "Price",
+              width: 130,
+              createDefaultValue: (i: any, j: any) : string => [i, j].join(),
+            },
+            {
+              field: "quantity",
+              headerName: "Quantity",
+              width: 130,
+              createDefaultValue: (i: any, j: any) : string => [i, j].join(),
+            },
+          ]}
+        />
+      </Box>
     </Box>
   );
 };
