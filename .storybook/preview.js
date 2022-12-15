324a02ecf9f05a742254d36f8367643ae7632395
sujan-dahal <dahalsujanusa@gmail.com>
2022-12-14 19:55:06 -0600

    add controls to storybook component
    https://storybook.js.org/blog/material-ui-in-storybook/

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -30,6 +30,7 @@ export const decorators = [withMuiTheme];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true, // Adds the description and default columns
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
