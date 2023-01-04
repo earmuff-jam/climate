85841ecf49962acefa78e2090ef174d20f527ba9
Mohit <44540219+mohit2530@users.noreply.github.com>
2023-01-04 07:54:31 -0600

    Adds new layout and styles (#31)
    - adds app bar to display navigation instead of nav bar. we are doing this to make it more uniform on a mobile view.
    - words changed in idea component
    - cleanup on layout.js component to remove navbar and add appbar
    - applying inner padding for roi calculator but this needs to be redesigned
    - cleanup on subscribe component as users can save first name and last name
    
    at the moment there is no requirement to have a user matching profile to a subscribe profile. however, there must be a link associated with it. we might need to think how we are going to apply roles and permissions if the user is not a valid user. at the moment without a validity it is not possible to view categories and item lists.

---
+++ components/NavBar/AppBar.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Stack } from '@mui/system';
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+import ListItem from "@mui/material/ListItem";
+import MailIcon from "@mui/icons-material/Mail";
+import SourceIcon from "@mui/icons-material/Source";
+import WebStoriesIcon from "@mui/icons-material/WebStories";
+import FeedbackRoundedIcon from '@mui/icons-material/FeedbackRounded';
+import { Typography } from '@mui/material';
+
+const currentRoutes = [
+    {
+        title: "Home",
+        link: "/",
+        icon: <WebStoriesIcon />,
+        isSelected: (val: string): boolean => val === "/",
+    },
+    {
+        title: "Subscribe",
+        link: "/subscribe",
+        icon: <MailIcon />,
+        isSelected: (val: string): boolean => val === "/subscribe",
+    },
+    {
+        title: "Stuff",
+        link: "/stuff",
+        icon: <SourceIcon />,
+        isSelected: (val: string): boolean => val === "/stuff",
+    },
+    {
+        title: "Feedback",
+        link: "/feedback",
+        icon: <FeedbackRoundedIcon />,
+        isSelected: (val: string): boolean => val === "/feedback",
+    }
+];
+
+const NavMenuBar = () => {
+
+    const { pathname } = useRouter();
+    const supabase = useSupabaseClient();
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar 
+            position="static"
+            color='secondary'
+            >
+                <Toolbar>
+                    <IconButton
+                        size="small"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Stack
+                        direction="row"
+                        gap={1}
+                        sx={{ flexGrow: 1 }}
+                    >
+                        {currentRoutes.map((route, index) => {
+                            return (
+                                <ListItem
+                                    key={route.title}
+                                    dense
+                                    disablePadding
+                                    disableGutters
+                                >
+                                    <Link href={route.link}>
+                                        <Typography>
+                                            {route.title}
+                                        </Typography>
+                                    </Link>
+                                </ListItem>
+                            );
+                        })}
+
+                    </Stack>
+
+                    <Button
+                        onClick={async () => {
+                            await supabase.auth.signOut();
+                        }}
+                        color="inherit"
+                    >
+                        Sign out
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+export default NavMenuBar;
