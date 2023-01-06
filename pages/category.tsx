6e3e4034bf78d2281931172397db1617375d9388
mohit paudyal <mohit.paudyal@gmail.com>
2023-01-06 09:06:19 -0600

    adds category list from api
    - prevents category tag atm due to tsx issue.
    - displays category list by user

--- pages/category.tsx
+++ pages/category.tsx
@@ -1,15 +1,109 @@
-import React, { useState, useEffect } from 'react';
+
+import React, {
+    useState,
+    useEffect,
+} from "react";
+
+
+import BaseTable, {
+    AutoResizer,
+    Column,
+} from 'react-base-table'
+import 'react-base-table/styles.css'
 
 import {
+    useUser,
+    useSession,
     useSupabaseClient
 } from "@supabase/auth-helpers-react";
 
-import { Grid } from '@mui/material';
-import CategoryPage from "../components/CategoryPage/CategoryPage";
+import { Box } from "@mui/system";
+import { Chip } from "@mui/material";
 
 const Category = () => {
 
+    const user = useUser();
+    const session = useSession(); // user values are here too
     const supabaseClient = useSupabaseClient();
+
+    const DisplayTag = (props: any) => {
+        const { tags } = props;
+        return (
+            <>
+                {
+                    tags?.map((el: any) => (
+                        <Chip key={el.id} label={el.item_name}>
+                            {el.item_name}
+                        </Chip>
+                    ))
+                }
+            </>
+        )
+    };
+
+    const columns = [
+        {
+            key: "id",
+            title: "Category Id",
+            dataKey: "id",
+            width: 200,
+            resizable: true,
+            sortable: true,
+            editable: true,
+            frozen: Column.FrozenDirection.LEFT
+        },
+        {
+            key: "cat_type",
+            title: "Category Type",
+            dataKey: "category_type",
+            width: 300,
+            resizable: true,
+            sortable: true,
+            editable: true,
+            frozen: Column.FrozenDirection.LEFT
+        },
+        {
+            key: "cat_name",
+            title: "Category Name",
+            dataKey: "category_name",
+            width: 300,
+            resizable: true,
+            sortable: true,
+            editable: true,
+            frozen: Column.FrozenDirection.LEFT
+        },
+        {
+            key: "cat_desc",
+            title: "Category Description",
+            dataKey: "category_description",
+            width: 600,
+            resizable: true,
+            sortable: true,
+            editable: true,
+            frozen: Column.FrozenDirection.LEFT
+        },
+        {
+            key: "cat_tag",
+            title: "Category Tags",
+            dataKey: "category_tag",
+            width: 250,
+            resizable: true,
+            sortable: true,
+            editable: true,
+            cellRenderer: ({ cellData: tags }) => <DisplayTag tags={tags} />
+        },
+        {
+            key: "created_on",
+            title: "Last updated Date",
+            dataKey: "created_on",
+            width: 400,
+            resizable: true,
+            sortable: true,
+            editable: true,
+            frozen: Column.FrozenDirection.RIGHT
+        },
+    ];
+
     const [datasets, setDatasets] = useState<any | null>([]);
 
     const fetchCategoryList = async () => {
@@ -32,19 +126,19 @@ const Category = () => {
     }, []);
 
     return (
-        <>
-            <Grid
-                container
-                alignItems="center"
-            >
-                <Grid item xs={6} md={12}>
+        <Box style={{ width: "100vw", height: "100vh" }}>
+            <AutoResizer>
+                {({ width, height }) => (
+                    <BaseTable
+                        columns={columns}
+                        data={datasets}
+                        width={width}
+                        height={height}
+                    />
+                )}
+            </AutoResizer>
+        </Box>
 
-                </Grid>
-                <Grid item xs={12} md={12}>
-                    {datasets && <CategoryPage datasets={datasets} />}
-                </Grid>
-            </Grid>
-        </>
     )
 };
 
