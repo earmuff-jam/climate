ff31bea3793e3d161023fda552a1d7352c8d7066
mohit paudyal <mohit.paudyal@gmail.com>
2023-01-20 06:42:03 -0600

    complete-add-category-cleanup

--- components/CategoryPage/AddItem.tsx
+++ components/CategoryPage/AddItem.tsx
@@ -31,55 +31,66 @@ import {
     useSupabaseClient
 } from "@supabase/auth-helpers-react";
 import { MENU_PROPS, TYPE_OPTIONS } from "./constants";
+import { now } from "moment";
 
-const AddCategory = (props: any) => {
+const AddItem = (props: any) => {
 
     const supabaseClient = useSupabaseClient();
-    const { addCategorySelection, setAddCategorySelection } = props;
+    const { addItemSelection, setAddItemSelection, rowData = [] } = props;
 
-    const [type, setType] = useState('');
     const [name, setName] = useState('');
     const [desc, setDesc] = useState('');
+    const [quantity, setQuantity] = useState(1);
 
+    const [useBy, setUseBy] = useState('');
     const [barCodeValue] = useState(uuid());
+
     const [error, setError] = useState(false);
     const [tags, setTags] = useState<string[]>([]);
     const [tagOptions, setTagOptions] = useState<string[] | undefined>([]);
 
-    const handleSetType = (e: SelectChangeEvent<typeof type>) => {
-        setType(e.target.value);
-    }
     const handleSetName = (val: string) => setName(val);
     const handleSetDesc = (val: string) => setDesc(val);
+    const handleSetQuantity = (val: number) => setQuantity(val);
+
     const handleSetTags = (event: SelectChangeEvent<typeof tags>) => {
         const { target: { value } } = event;
         setTags(
             typeof value === 'string' ? value.split(',') : value,
         );
     };
-
+    console.log(rowData);   
     const saveToDb = async () => {
         const { data } = await supabaseClient.from('profiles').select('*').limit(1);
+        // always is going to be the logged in user
         const user_id = data && data[0].id;
-        console.log(user_id, name, type, desc, barCodeValue, user_id);
 
-        const { error } = await supabaseClient
-            .from('category')
-            .insert({ category_type: type.toUpperCase(),
-                category_name: name,
-                category_description: desc,
-                barcode: barCodeValue,
-                created_by: user_id,
-                sharable_groups: [user_id],
-    });
+        // create a function in supabase that returns the category id of the category that the row data is in. since the row data doesn't have the id.
+        const category_id = '1';
+
+        const { data: response, error } = await supabaseClient
+            .from('item')
+            .insert([
+                {
+                    item_name: name,
+                    item_description: desc,
+                    quantity:  quantity,
+                    use_by_date: useBy,
+                    category_id: category_id,
+                    created_by: user_id,
+                    sharable_groups: [user_id]
+                }
+            ]);
+
         if (error) { console.log(error) }
         else {
-            setAddCategorySelection(false);
+            console.log(response);
+            setAddItemSelection(false);
         }
     };
 
     const handleSubmit = () => {
-        if ((name === '' || undefined) || (type === '' || undefined)) {
+        if ((name === '' || undefined) ) {
             setError(true);
             return;
         }
@@ -102,8 +113,8 @@ const AddCategory = (props: any) => {
     return (
         <>
             <Dialog
-                open={addCategorySelection}
-                onClose={() => setAddCategorySelection(false)}
+                open={addItemSelection}
+                onClose={() => setAddItemSelection(false)}
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
             >
@@ -139,7 +150,7 @@ const AddCategory = (props: any) => {
                                 title="Existing changes will be discarded"
                             >
                                 <IconButton
-                                    onClick={() => setAddCategorySelection(false)}
+                                    onClick={() => setAddItemSelection(false)}
                                 >
                                     <CloseRoundedIcon />
                                 </IconButton>
@@ -160,32 +171,7 @@ const AddCategory = (props: any) => {
                                 padding={2}
                             >
                                 <Grid item xs={5}>
-                                    <FormControl fullWidth>
-                                        <InputLabel
-                                            id='category-type-input'
-                                        >
-                                            Type
-                                        </InputLabel>
-                                        <Select
-                                            labelId="category-type-input-label"
-                                            id='category-type-input-id'
-                                            value={type}
-                                            onChange={handleSetType}
-                                            input={
-                                                <OutlinedInput id='select-chip-input' label='Chip'
-                                                />
-                                            }
-                                            MenuProps={MENU_PROPS}
-                                        >
-                                            {TYPE_OPTIONS?.map((item) => (
-                                                <MenuItem
-                                                    key={item.id}
-                                                    value={item.name}>
-                                                    {item.name}
-                                                </MenuItem>
-                                            ))}
-                                        </Select>
-                                    </FormControl>
+                                    NEED SOMETHING HERE ?
                                 </Grid>
 
                                 <Grid item xs={5}>
@@ -284,4 +270,4 @@ const AddCategory = (props: any) => {
     )
 };
 
-export default AddCategory;
+export default AddItem;
--- components/CategoryPage/constants.tsx
+++ components/CategoryPage/constants.tsx
@@ -1,11 +1,10 @@
-
 export const ITEM_HEIGHT: number = 48;
 export const ITEM_PADDING_TOP: number = 8;
 
 export const MENU_PROPS = {
     PaperProps: {
         style: {
-            maxHeight:ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
             width: 250,
         },
     },
@@ -21,3 +20,13 @@ export const TYPE_OPTIONS = [
         name: 'Business',
     }
 ];
+
+export const regularAndHigherScreenSx = {
+    width: "100vm",
+    height: "100vh",
+};
+
+export const smallScreenSx = {
+    width: '70rem',
+    height: '70rem',
+};
