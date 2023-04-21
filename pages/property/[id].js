f3cd0809dc341cab7387c7a52d7fbbf1abc904ae
sujan-dahal <dahalsujanusa@gmail.com>
2023-04-20 23:43:24 -0500

    start revamp property layout

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,67 +1,26 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Card } from "@mui/material";
 import { Share } from "@mui/icons-material";
 import { useRouter } from "next/router";
 import default_img_property from "../../public/default_img_property.jpeg";
-import Image from "next/image";
 import PropertyReport from "../../components/PropertiesComponent//PropertyReport";
 import PaymentHistory from "../../components/PropertiesComponent/PaymentHistory";
 import { payments } from "../../containers/HomeContainer/constants";
 import { useBuildPropertyDetails } from "../../containers/PropertiesContainer/PropertyContainerHooks";
+import BasicImageCoverArt from "../../components/ImageCoverArt/BasicImageCoverArt";
 
 const PropertyDetails = () => {
   const router = useRouter();
   const route_id = router.query.id;
   const { property } = useBuildPropertyDetails(route_id);
   return (
-    <Box sx={{ flexGrow: 1, p: 2 }}>
-      <Card sx={{ display: "flex", flexDirection: "column" }}>
-        <CardMedia alt={property.name}>
-          <Image
-            src={property.image || default_img_property}
-            width={768}
-            height={200}
-            alt="a generic image of a property or the specific image of the property that the owner has uploaded"
-            objectFit="contain" // or objectFit="cover"
-          />
-        </CardMedia>
-        <CardContent sx={{ flexGrow: 1 }}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {property.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {`${property.address}`}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {`${property.sqft} sq. ft. | ${property.numberofbedrooms} beds | ${property.numberofbathrooms} baths`}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {`Built in ${property.yearbuilt} | ${property.garage} car garage`}
-          </Typography>
-        </CardContent>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            px: 2,
-            pb: 2,
-          }}
-        >
-          <Typography variant="caption" color="text.secondary">
-            {`${property.address}`}
-          </Typography>
-          <Button variant="contained" color="primary" startIcon={<Share />}>
-            Share with Friends
-          </Button>
-        </Box>
-      </Card>
+    <Box>
+      <BasicImageCoverArt
+        property={property}
+        imageProps={{
+          src: property.image || default_img_property,
+          alt: "a generic image of a property or the specific image of the property that the owner has uploaded",
+        }}
+      />
       <>tenant details and history </>
       <PaymentHistory payments={payments} />
       <PropertyReport property={property} />
