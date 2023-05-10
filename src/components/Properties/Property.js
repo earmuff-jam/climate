68d9f56260128cbd24928140389409377c37969c
mohit paudyal <mohit.paudyal@gmail.com>
2023-05-09 21:38:34 -0500

    Introduce tailwind && New Migrations
    1. Introduce user profile config hook to perform CRUD.
    2. Introduce ProfileChips components to initiate profile chip icons where necessary.
    3. Prevent empty property list creating red alerts in property page.
    4. Apply CSS Refactor for class vs classNames in file Property.js
    5. Introduce tanstack query to retrieve data and / or load data during properties load.
    6. Introduce tanstack query to retrieve data for properties - invalidate ability to submit properties
    7. Introduce new supabase migrations that better support the supabase auth system.
    8. Seed some fake data for seed.sql.

--- src/components/Properties/Property.js
+++ src/components/Properties/Property.js
@@ -4,44 +4,44 @@ const Property = ({ property }) => {
   console.log(property);
   return (
     <>
-      <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-        <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-          <div class="flex flex-col items-start justify-between">
-            <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
+      <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+        <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+          <div className="flex flex-col items-start justify-between">
+            <h2 classNameName="text-lg font-medium text-gray-900" id="slide-over-title">
               {property?.title}
             </h2>
             <img
               src={"/images/" + `${property?.id}` + ".jpg"}
-              alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
-              class="h-full w-full object-cover object-center"
+              alt="Image of property"
+              classNameName="h-full w-full object-cover object-center"
             />
           </div>
 
-          <div class="mt-8">
-            <div class="flow-root">
-              <ul role="list" class="-my-6 divide-y divide-gray-200">
-                <li class="flex py-6">
-                  <div class="ml-4 flex flex-1 flex-col">
+          <div className="mt-8">
+            <div className="flow-root">
+              <ul role="list" className="-my-6 divide-y divide-gray-200">
+                <li className="flex py-6">
+                  <div className="ml-4 flex flex-1 flex-col">
                     <div>
-                      <div class="flex justify-between text-base font-medium text-gray-900">
+                      <div className="flex justify-between text-base font-medium text-gray-900">
                         <h3>
                           <a href="#">{property?.property_type}</a>
                         </h3>
-                        <p class="ml-4">$ {property?.rent_amount}</p>
+                        <p className="ml-4">$ {property?.rent_amount}</p>
                       </div>
-                      <p class="mt-1 text-sm text-gray-500">
+                      <p className="mt-1 text-sm text-gray-500">
                         {property?.lease_term} months
                       </p>
                     </div>
-                    <div class="flex flex-1 items-end justify-between text-sm">
-                      <p class="text-gray-500">
+                    <div className="flex flex-1 items-end justify-between text-sm">
+                      <p className="text-gray-500">
                         {property?.bathrooms} Bath {property?.bedrooms} Bed
                       </p>
 
-                      <div class="flex">
+                      <div className="flex">
                         <button
                           type="button"
-                          class="font-medium text-indigo-600 hover:text-indigo-500"
+                          className="font-medium text-indigo-600 hover:text-indigo-500"
                         >
                           Add to wishlist
                         </button>
@@ -49,29 +49,29 @@ const Property = ({ property }) => {
                     </div>
                   </div>
                 </li>
-                <li class="flex py-6">
-                  <div class="ml-4 flex flex-1 flex-col">
+                <li className="flex py-6">
+                  <div className="ml-4 flex flex-1 flex-col">
                     <div>
-                      <div class="flex justify-between text-base font-medium text-gray-900">
+                      <div className="flex justify-between text-base font-medium text-gray-900">
                         <h3>
                           <a href={`mailto: ${property?.contact_email}`}>
                             {property?.contact_name}
                           </a>
                         </h3>
-                        <p class="ml-4">{property?.contact_phone}</p>
+                        <p className="ml-4">{property?.contact_phone}</p>
                       </div>
-                      <p class="mt-1 text-sm text-gray-500">
+                      <p className="mt-1 text-sm text-gray-500">
                         {property?.contact_email}
                       </p>
                     </div>
-                    <div class="flex flex-1 items-end justify-between text-sm">
-                      <p class="text-gray-500">
+                    <div className="flex flex-1 items-end justify-between text-sm">
+                      <p className="text-gray-500">
                         {property?.square_footage} sq. ft
                       </p>
-                      <div class="flex">
+                      <div className="flex">
                         <button
                           type="button"
-                          class="font-medium text-indigo-600 hover:text-indigo-500"
+                          className="font-medium text-indigo-600 hover:text-indigo-500"
                         >
                           {property?.availability_dates}
                         </button>
@@ -84,30 +84,30 @@ const Property = ({ property }) => {
           </div>
         </div>
 
-        <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
-          <div class="flex justify-between text-base font-medium text-gray-900">
+        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+          <div className="flex justify-between text-base font-medium text-gray-900">
             <p>Amenities</p>
           </div>
-          <p class="mt-0.5 text-sm text-gray-500">
+          <p className="mt-0.5 text-sm text-gray-500">
             {property?.amenities}
             <br />
             {property?.pet_policy}
             <br />
             {property?.nearby_locations}
           </p>
-          <div class="mt-6">
+          <div className="mt-6">
             <a
               href="#"
-              class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+              className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
             >
               Contact Property Manager
             </a>
           </div>
-          <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
+          <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
             <p>
               <button
                 type="button"
-                class="font-medium text-indigo-600 hover:text-indigo-500"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
               >
                 Share property
                 <span aria-hidden="true"> &rarr;</span>
