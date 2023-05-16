ea62f47f3db176a42419fa0f42de3fe8d6fe31f0
mohit paudyal <mohit.paudyal@gmail.com>
2023-05-16 16:43:38 -0500

    Adds ability to create maintenance forms
    1. Adds ability to create maintenance forms.
    2. Adds ability to navigate to maintenance forms.
    3. Adds constants for form fields for maintenance form.
    4. Adds new component EmptyMaintenance.js
    5. Adds default color of black to text.
    6. Adds List.js where property owners can create maintenance forms for respective properties.
    7. Adds List of maintenance forms display.

--- supabase/migrations/20230516212235_load_maintenance_form.sql
+++ supabase/migrations/20230516212235_load_maintenance_form.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS inspection;
 CREATE TABLE IF NOT EXISTS inspection
 (
     id               UUID PRIMARY KEY         NOT NULL DEFAULT uuid_generate_v4(),
-    property_id      UUID                     NOT NULL REFERENCES properties (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    property_id      UUID                     NOT NULL REFERENCES public.properties (id) ON UPDATE CASCADE ON DELETE CASCADE,
     name             TEXT                     NOT NULL,
     inspection_date  TIMESTAMP WITH TIME ZONE NOT NULL,
     inspection_type  TEXT                     NOT NULL,
     general_comments TEXT,
     signature        TEXT                     NOT NULL,
-    created_by       UUID                     NOT NULL REFERENCES profiles (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    created_by       UUID                     NOT NULL REFERENCES public.profiles (id) ON UPDATE CASCADE ON DELETE CASCADE,
     created_on       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
     updated_by       UUID,
     updated_on       TIMESTAMP WITH TIME ZONE,
